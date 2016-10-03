9e63627682eee79ae9c87979f45f913d3e68ebbb
Natalia Carrillo Blanco <ncarrill@gamma29.aulas.gsyc.urjc.es>
2016-10-03 11:00:09 +0200

    He empezdo ejercicio 3

--- smallsmilhandler.py
+++ smallsmilhandler.py
@@ -1,4 +1,26 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
+class SmallSMILHandler(ContentHandler):
 
+    def __init(self):
+    """Inicializar variables"""
+        self.width = ""
+        self.height = ""
+        self.background-color = ""
+
+    def startElement(self):
+    """Metodo que se llama cuando se abre una etiqueta"""
+        if name == 'root-layout':
+            self.widht
+            print (self.width)
+        
+        elif name == 'region':
+        
+        elif name == 'img':
+        
+        elif name == 'audio':
+        
+        elif name == 'textstream':
+        
+ 
