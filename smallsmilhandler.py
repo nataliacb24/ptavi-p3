005fdf76bd966676cedf1ba8506b2429e266b7cf
Natalia Carrillo Blanco <ncarrill@gamma37.aulas.gsyc.urjc.es>
2016-10-03 17:31:09 +0200

    Simplificado ejercicio 3 pratica 3

--- smallsmilhandler.py
+++ smallsmilhandler.py
@@ -9,51 +9,22 @@ class SmallSMILHandler(ContentHandler):
 
     def __init__(self):
 
-        self.root_layout = {}
-        self.region = {}
-        self.img = {}
-        self.audio = {}
-        self.textstream = {}
         self.ListaDicc = []
+        self.DiccAtrib = {'root-layout': ['width', 'height', 'background-color'],
+                          'region': ['id', 'top', 'bottom', 'left', 'rigth'],
+                          'img': ['src', 'region', 'begin', 'dur'],
+                          'audio': ['src', 'begin', 'dur'],
+                          'textstream': ['src', 'region']}
 
     def startElement(self, name, attrs):
-
-        if name == 'root-layout':
-            self.root_layout = {'Tag': 'root-layout',
-                                'width': attrs.get('width',""),
-                                'height': attrs.get('height', ""),
-                                'background-color': attrs.get('background-color',"")}
-            self.ListaDicc.append(self.root_layout)
-
-        elif name == 'region':
-            self.region = {'Tag': 'region',
-                           'id': attrs.get('id',""),
-                           'top': attrs.get('top', ""),
-                           'bottom': attrs.get('bottom', ""),
-                           'left': attrs.get('left', ""),
-                           'rigth': attrs.get('right', "")}
-            self.ListaDicc.append(self.region)
-
-        elif name == 'img':
-            self.img = {'Tag': 'img',
-                        'src': attrs.get('src', ""),
-                        'region': attrs.get('region', ""),
-                        'begin': attrs.get('begin',""),
-                        'dur': attrs.get('dur',"")}
-            self.ListaDicc.append(self.img)
-
-        elif name == 'audio':
-            self.audio ={'Tag': 'audio',
-                         'src': attrs.get('src',""),
-                         'begin': attrs.get('begin',""),
-                         'dur': attrs.get('dur',"")}
-            self.ListaDicc.append(self.audio)
-
-        elif name == 'textstream':
-            self.textstream = {'Tag': 'teststream',
-                               'src': attrs.get('src',""),
-                               'region': attrs.get('region',"")}
-            self.ListaDicc.append(self.textstream)
+    
+        dicc = {}
+        
+        if name in self.DiccAtrib:
+            dicc = {'Tag': name}
+            for atribute in self.DiccAtrib[name]:
+                dicc[atribute] = attrs.get(atribute,"")
+            self.ListaDicc.append(dicc)
 
     def get_tags(self):
     #Metodo que me imprime la lista de diccionarios
