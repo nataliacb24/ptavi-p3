69c5dbfa5f8770a37d4c492df1a9543eaf258d66
Natalia Carrillo Blanco <ncarrill@gamma28.aulas.gsyc.urjc.es>
2016-10-10 16:23:50 +0200

    Terminado ejercicio 5 y 6 practica 3

--- karaoke.py
+++ karaoke.py
@@ -3,64 +3,64 @@
 
 import sys
 import json
-import urllib
+import urllib.request
 import smallsmilhandler
+
 from xml.sax import make_parser
 from xml.sax.handler import ContentHandler
 
 
 class KaraokeLocal():
 
-    def inicializador(self, fich):
-    
+    def __init__(self, fich):
+        #Inicializar
         parser = make_parser()
-        self.kHandler = smallsmilhandler.SmallSMILHandler()
-        parser.setContentHandler(self.kHandler)
+        kHandler = smallsmilhandler.SmallSMILHandler()
+        parser.setContentHandler(kHandler)
         parser.parse(open(fich))
-        self.datos_smil = self.kHandler.get_tags()
+        self.datos_smil = kHandler.get_tags()
 
 
     def __str__(self):
-
+        #Escribir datos
+        linea = ""
         for eti_dicc in self.datos_smil:
-            linea = ""
             etiqueta = eti_dicc['Tag']
             linea += etiqueta
 
             for atributo in eti_dicc:
-                value = eti_dicc[atributo]
-                linea += '\t' + atributo + "=" + value
-            print(linea)
+                if atributo != 'Tag' :
+                    value = eti_dicc[atributo]
+                    linea += '\t' + atributo + "=" + value + '\n'
+        return(linea)
 
 
-    def to_json(self, fich):
+    def do_json(self, fich):
         #Convertir un fichero smil en uno json
         jsonfich = json.dumps(self.datos_smil)
         nombrefich = fich.split('.')[0] + '.json'
-        with open(jsonfich, 'w') as fichjson:
+        with open(nombrefich, 'w') as fichjson:
             json.dump(jsonfich, fichjson)
-    
-    #def do_local(self, datos_smil):
 
-    #    for linea in self.datos_smil:
-    #        for atributo in linea.keys():
-    #            if atributo == "src":
-    #                if linea[atributo].split[':'][0] == 'http://':
-    #                    urllib.request.urlretrieve(linea[atributo], linea[atributo].split('/')[-1])
-    #                    linea[atributo] = linea[atributo].split('/')[-1]
-    #        print(atributo)
+
+    def do_local(self):
+        #Guardar en local
+        for linea in self.datos_smil:
+            for atributos in linea:
+                if linea[atributos][:7] == 'http://':
+                    url = linea[atributos].split('/')[-1]
+                    urllib.request.urlretrieve(linea[atributos], url)
+                    linea[atributos] = url
 
 
 if __name__ == "__main__":
-                   
-    try:
-        fich = sys.argv[1]
 
-    except IndexError:
+    fich = sys.argv[-1]
+    if len(sys.argv) == 2 and fich.split('.')[-1] == 'smil':
+        karaoke = KaraokeLocal(fich)
+        print(karaoke)
+        karaoke.do_json(fich)
+        karaoke.do_local()
+        print(karaoke)
+    else:
         sys.exit("Usage: python3 karaoke.py file.smil")
-    
-    karaoke = KaraokeLocal()
-    karaoke.inicializador(fich)
-    #print(karaoke)
-    karaoke.to_json(fich)
-#   karaoke.do_local()
