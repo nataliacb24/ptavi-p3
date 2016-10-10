8fdc5433787fa07db7177981a5ab1e1bdc29efc2
Natalia Carrillo Blanco <ncarrill@gamma28.aulas.gsyc.urjc.es>
2016-10-10 16:30:23 +0200

    PEP8 ejercicio 6 practica 3

--- karaoke.py
+++ karaoke.py
@@ -14,15 +14,16 @@ class KaraokeLocal():
 
     def __init__(self, fich):
         #Inicializar
+
         parser = make_parser()
         kHandler = smallsmilhandler.SmallSMILHandler()
         parser.setContentHandler(kHandler)
         parser.parse(open(fich))
         self.datos_smil = kHandler.get_tags()
 
-
     def __str__(self):
         #Escribir datos
+
         linea = ""
         for eti_dicc in self.datos_smil:
             etiqueta = eti_dicc['Tag']
@@ -34,17 +35,17 @@ class KaraokeLocal():
                     linea += '\t' + atributo + "=" + value + '\n'
         return(linea)
 
-
     def do_json(self, fich):
         #Convertir un fichero smil en uno json
+
         jsonfich = json.dumps(self.datos_smil)
         nombrefich = fich.split('.')[0] + '.json'
         with open(nombrefich, 'w') as fichjson:
             json.dump(jsonfich, fichjson)
 
-
     def do_local(self):
         #Guardar en local
+
         for linea in self.datos_smil:
             for atributos in linea:
                 if linea[atributos][:7] == 'http://':
