cbde58adc657a8c21f44f78efbac43ac52e1fe82
Natalia Carrillo Blanco <ncarrill@gamma26.aulas.gsyc.urjc.es>
2016-10-10 11:35:20 +0200

    Terminado ejercicio 4 practica 3

--- karaoke.py
+++ karaoke.py
@@ -1,8 +1,49 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
+import sys
+import json
+
 from xml.sax import make_parser
 from xml.sax.handler import ContentHandler
-import smallsmilhandler
+from smallsmilhandler import SmallSMILHandler
 
 
+def mifichero(datos_smil):
+
+    for eti_dicc in datos_smil:
+        linea = ""
+        etiqueta = eti_dicc['Tag']
+        linea += etiqueta
+
+        for atributo in eti_dicc:
+            value = eti_dicc[atributo]
+            linea += '\t' + atributo + "=" + value
+        print(linea)
+
+
+def conver_json(datos_smil):
+    #Convertir un fichero smil en uno json
+    smilfich = sys.argv[1]
+    nombrefich = open(smilfich.split('.')[0] + '.json', 'w')
+    jsonfich = json.dumps(datos_smil)
+    nombrefich.write(jsonfich)
+    nombrefich.close()
+
+
+if __name__ == "__main__":
+
+    try:
+        smilfich = sys.argv[1]
+
+    except IndexError:
+        sys.exit("Usage: python3 karaoke.py file.smil")
+
+    parser = make_parser()
+    kHandler = SmallSMILHandler()
+    parser.setContentHandler(kHandler)
+    parser.parse(open(smilfich))
+    datos_smil = kHandler.get_tags()
+
+    mifichero(datos_smil)
+    conver_json(datos_smil)
