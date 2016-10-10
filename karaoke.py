12f9150bb5b987592b9a256a9992b6bf5d92ab54
Natalia Carrillo Blanco <ncarrill@gamma26.aulas.gsyc.urjc.es>
2016-10-10 13:08:05 +0200

    A medias ejercicio 6

--- karaoke.py
+++ karaoke.py
@@ -4,58 +4,63 @@
 import sys
 import json
 import urllib
-
+import smallsmilhandler
 from xml.sax import make_parser
 from xml.sax.handler import ContentHandler
-from smallsmilhandler import SmallSMILHandler
 
 
-#def dow_local(datos_smil):
+class KaraokeLocal():
 
-#    for linea in datos_smil:
-#        for atributo in linea.keys():
-#            if atributo == "src":
-#                if linea[atributo].split[':'][0] == 'http://':
-#                    urllib.request.urlretrieve(linea[atributo], linea[atributo].split('/')[-1])
-#                    linea[atributo] = linea[atributo].split('/')[-1]
-#        print(atributo)
+    def inicializador(self, fich):
     
-def mifichero(datos_smil):
-
-    for eti_dicc in datos_smil:
-        linea = ""
-        etiqueta = eti_dicc['Tag']
-        linea += etiqueta
-
-        for atributo in eti_dicc:
-            value = eti_dicc[atributo]
-            linea += '\t' + atributo + "=" + value
-        print(linea)
+        parser = make_parser()
+        self.kHandler = smallsmilhandler.SmallSMILHandler()
+        parser.setContentHandler(self.kHandler)
+        parser.parse(open(fich))
+        self.datos_smil = self.kHandler.get_tags()
 
 
-def conver_json(datos_smil):
-    #Convertir un fichero smil en uno json
-    smilfich = sys.argv[1]
-    nombrefich = open(smilfich.split('.')[0] + '.json', 'w')
-    jsonfich = json.dumps(datos_smil)
-    nombrefich.write(jsonfich)
-    nombrefich.close()
+    def __str__(self):
+
+        for eti_dicc in self.datos_smil:
+            linea = ""
+            etiqueta = eti_dicc['Tag']
+            linea += etiqueta
+
+            for atributo in eti_dicc:
+                value = eti_dicc[atributo]
+                linea += '\t' + atributo + "=" + value
+            print(linea)
+
+
+    def to_json(self, fich):
+        #Convertir un fichero smil en uno json
+        jsonfich = json.dumps(self.datos_smil)
+        nombrefich = fich.split('.')[0] + '.json'
+        with open(jsonfich, 'w') as fichjson:
+            json.dump(jsonfich, fichjson)
+    
+    #def do_local(self, datos_smil):
+
+    #    for linea in self.datos_smil:
+    #        for atributo in linea.keys():
+    #            if atributo == "src":
+    #                if linea[atributo].split[':'][0] == 'http://':
+    #                    urllib.request.urlretrieve(linea[atributo], linea[atributo].split('/')[-1])
+    #                    linea[atributo] = linea[atributo].split('/')[-1]
+    #        print(atributo)
 
 
 if __name__ == "__main__":
-
+                   
     try:
-        smilfich = sys.argv[1]
+        fich = sys.argv[1]
 
     except IndexError:
         sys.exit("Usage: python3 karaoke.py file.smil")
-
-    parser = make_parser()
-    kHandler = SmallSMILHandler()
-    parser.setContentHandler(kHandler)
-    parser.parse(open(smilfich))
-    datos_smil = kHandler.get_tags()
-
-#    dow_local(datos_smil)
-    mifichero(datos_smil)
-    conver_json(datos_smil)
+    
+    karaoke = KaraokeLocal()
+    karaoke.inicializador(fich)
+    #print(karaoke)
+    karaoke.to_json(fich)
+#   karaoke.do_local()
